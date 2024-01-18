e03531c52c9976f4e09e93b3bf694dd722267c68
thucvinguyen <thucvinguyen@gmail.com>
2024-01-18 21:29:35 +0700

    my first commit

---
+++ intro-react/src/App.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+  let color = count > 0 ? "green" : count < 0 ? "red" : "black";
+
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
+  const resetCount = () => setCount(0);
+
+  return (
+    <div className="container">
+      <h1>Counter</h1>
+      <span style={{ color }} id="value">
+        {count}
+      </span>
+      <div className="btn-group">
+        <button onClick={decrement} className="btn btn-decrease">
+          DECREASE
+        </button>
+        <button onClick={resetCount} className="btn btn-reset">
+          RESET
+        </button>
+        <button onClick={increment} className="btn btn-increase">
+          INCREASE
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
